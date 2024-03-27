cb72537ca991b333620b568bd0510f9f6a50036a
seadosky <157003372+seadosky@users.noreply.github.com>
2024-03-27 18:12:39 +0900

    .

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function(){
     
+    $('.header .gnb .gnb_open').on('click', function(){
+        $('.header').addClass('menu_open')
+    })
+    $('.header .gnb .gnb_close').on('click', function(){
+        $('.header').removeClass('menu_open')
+    })
+
     const visual_swiper = new Swiper('.visual .swiper', { 
 
+    effect: "fade",
+
 	autoplay: { 
-		delay: 2500,
+		delay: 5000,
 		disableOnInteraction: true,
 	},
 
 	loop: true, 
 
     });
+
+    const exh_swiper = new Swiper('.exh .swiper', { 
+
+	autoplay: {  
+		delay: 2500,
+		disableOnInteraction: true,
+	},
+
+	loop: true,  
+
+	navigation: {  
+		nextEl: '.button_prev',  
+		prevEl: '.button_next',  
+	},
+
+});
 
 })//document.ready
