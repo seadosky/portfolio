bf5c8554cbd546e80e8b67367045a2eebaac49a5
seadosky <157003372+seadosky@users.noreply.github.com>
2024-03-27 13:23:36 +0900

    .

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     
+    const visual_swiper = new Swiper('.visual .swiper', { 
+
+	autoplay: { 
+		delay: 2500,
+		disableOnInteraction: true,
+	},
+
+	loop: true, 
+
+    });
+
 })//document.ready
