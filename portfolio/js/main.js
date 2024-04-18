c6b764332a6682051b45c1fa856c2a50246a72de
seadosky <157003372+seadosky@users.noreply.github.com>
2024-04-18 18:02:22 +0900

    .

---
+++ portfolio/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    const swiper = new Swiper('.swiper', { 
+        //direction: "vertical",
+
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	    spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+        mousewheel: true,
+	
+    });
+})
