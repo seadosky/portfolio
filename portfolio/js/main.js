03931984a203a8cbdc8243d50ef0d65c4ef378c2
seadosky <157003372+seadosky@users.noreply.github.com>
2024-04-19 11:13:25 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
     const swiper = new Swiper('.swiper', { 
         //direction: "vertical",
 
-        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	    spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+        slidesPerView: 1,
         mousewheel: true,
         on: {
             slideChange: function(){
